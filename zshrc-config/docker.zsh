fa213086a2c81ffbe090e71eb7c693c5696fac73
shytypes <abbaadamu302@gmail.com>
2025-07-21 23:18:37 +0100

    feat(zshrc):add docker bash helper methods for bitcoin service

--- zshrc-config/docker.zsh
+++ zshrc-config/docker.zsh
@@ -1,3 +1,21 @@
 export DEV_SERVICES="$HOME/.config/dev-services"
 export COLIMA_HOME="$HOME/.colima"
 export DOCKER_HOST="unix://${COLIMA_HOME}/default/docker.sock"
+
+#========================== Helper functions =================================
+#------- bitcoin
+d-btc-up(){
+docker compose --file="$DEV_SERVICES/bitcoin/docker-compose.yaml" up -d
+CONTAINER_ID=$(docker compose --file="$DEV_SERVICES/bitcoin/docker-compose.yaml" ps -q bitcoin)
+
+# Export to a file/env var
+echo "export BTC_CONTAINER_ID=$CONTAINER_ID" > "$HOME/.btc_container_env"
+echo "Container ID: $CONTAINER_ID"
+echo "written BTC_CONTAINER_ID to $HOME/.btc_container_env"
+}
+
+d-btc-nuke() {
+  source ~/.btc_container_env
+  container_id=$(docker rm --volumes --force "$BTC_CONTAINER_ID")
+  echo "Nuked container 💣: $container_id"
+}
